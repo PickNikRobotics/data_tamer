6b111fc0779639f9de1a9c925cb4818b2f090e4b
Davide Faconti <davide.faconti@gmail.com>
2023-12-14 20:51:36 +0100

    fix example

--- data_tamer/examples/custom_types.cpp
+++ data_tamer/examples/custom_types.cpp
@@ -20,36 +20,29 @@ int main()
   auto channel = ChannelsRegistry::Global().getChannel("my_channel");
 
   // Register as usual
-  auto pointA = channel->createLoggedValue<Point3D>("pointA");
-  Point3D pointB;
-  channel->registerValue<Point3D>("pointB", &pointB);
-
-  // Non-trivial types like Pose are also supported
+  Point3D point;
   Pose pose;
-  channel->registerValue("my_pose", &pose);
-
-  // std::vectors can be registered as long as their size will NEVER change
-  // It is your responsibility to guarantee that!
   std::vector<Point3D> points_vect(5);
-  channel->registerValue("points", &points_vect);
-
-  // std::array is also supported (safer than std::vector due to constant size)
   std::array<int32_t, 3> value_array;
+
+  channel->registerValue<Point3D>("point", &point);
+  channel->registerValue("pose", &pose);
+  channel->registerValue("points", &points_vect);
   channel->registerValue("value_array", &value_array);
 
   // Print the schema to understand how they are serialized
   std::cout << channel->getSchema() << std::endl;
 
-  // Note has the size of the message is almost the same as the raw data
-  size_t expected_size = sizeof(double) * 3 +                            // pointA
-                         sizeof(double) * 3 +                            // pointB
-                         sizeof(double) * 7 +                            // my_pose
-                         sizeof(uint32_t) + 5 * (sizeof(double) * 3) +   //points_vect
+  // Note has the size of the message is almost the same as the raw data.
+  // The only overhead is the size of points_vect
+  size_t expected_size = sizeof(double) * 3 +                            // point
+                         sizeof(double) * 7 +                            // pose
+                         sizeof(uint32_t) + 5 * (sizeof(double) * 3) +   // points_vect and its size
                          sizeof(int32_t) * 3;                            // value_array
 
   channel->takeSnapshot();
   std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
   std::cout << "\nMessage size: " << dummy_sink->latest_snapshot.payload.size()
-            << " exepcted: " << expected_size << std::endl;
+            << " expected: " << expected_size << std::endl;
 }
